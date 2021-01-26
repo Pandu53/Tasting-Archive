e07a589d1e813b511da3ae9fbb8a07c186bc64f5
Kevin Pandura <pandu.kev.kp@gmail.com>
2021-01-26 22:10:28 +0100

    style: :lipstick: made the tasting cards in tasting overview responsive

--- client/whiskey-tasting-frontend/src/components/TastingCard.vue
+++ client/whiskey-tasting-frontend/src/components/TastingCard.vue
@@ -2,14 +2,48 @@
   <div class="tasting-card">
     <table width="100%">
       <tr class="header-row">
-       <td><h4>Das {{tasting.id}}te Tasting am:</h4></td>
-       <td></td>
-       <td><h4>{{tasting.date}}.</h4></td>
+        <div class="row">
+          <div class="col-sm-6">
+            <h4>Das {{ tasting.id }}te Tasting am:</h4>
+          </div>
+          <div  class="col-sm-6">
+            <h4>{{ tasting.date }}.</h4>
+          </div>
+        </div>
       </tr>
       <tr v-if="tastingWhiskeys.length > 0">
-        <td><div class="row"><div class="col-4">1ter Platz (Rating:{{tastingWhiskeys[0].total_rating}}): </div><div class="col-8"> {{tastingWhiskeys[0].name}}</div></div></td>
-        <td><div class="row"><div class="col-4">2ter Platz (Rating:{{tastingWhiskeys[1].total_rating}}): </div><div class="col-8"> {{tastingWhiskeys[1].name}}</div></div></td>
-        <td><div class="row"><div class="col-4">3ter Platz (Rating:{{tastingWhiskeys[2].total_rating}}): </div><div class="col-8"> {{tastingWhiskeys[2].name}}</div></div></td>
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="row">
+              <div class="col-4">
+                <div class="fat">
+                  1ter Platz (Rating:{{ tastingWhiskeys[0].total_rating }}):
+                </div>
+              </div>
+              <div class="col-8">{{ tastingWhiskeys[0].name }}</div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="row">
+              <div class="col-4">
+                <div class="fat">
+                  2ter Platz (Rating:{{ tastingWhiskeys[1].total_rating }}):
+                </div>
+              </div>
+              <div class="col-8">{{ tastingWhiskeys[1].name }}</div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="row">
+              <div class="col-4">
+                <div class="fat">
+                  3ter Platz (Rating:{{ tastingWhiskeys[2].total_rating }}):
+                </div>
+              </div>
+              <div class="col-8">{{ tastingWhiskeys[2].name }}</div>
+            </div>
+          </div>
+        </div>
       </tr>
     </table>
   </div>
@@ -20,18 +54,18 @@
 export default {
   name: "tastingcard",
   props: ["tasting"],
-  data: function (){
+  data: function () {
     return {
       tastingWhiskeys: [],
-      loaded: false
-    }
+      loaded: false,
+    };
   },
-  created:function () {
+  created: function () {
     this.getWhiskeysForTasting();
   },
-  methods:{
-    getWhiskeysForTasting: function(){
-            this.loaded = false;
+  methods: {
+    getWhiskeysForTasting: function () {
+      this.loaded = false;
       fetch("http://localhost:3000/whiskeys/tasting/" + this.tasting.id, {
         "Content-Type": "application/json",
       })
@@ -45,32 +79,28 @@ export default {
           console.log(data);
           this.loaded = true;
         });
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
-
 .tasting-card {
-     margin-top: 30px;
-    padding: 0 20px;
-    border-style: solid;
-    border-width: 2px 0;
-    border-color: rgba(255, 235, 205, 0.288);
-    background-color: rgba(15, 1, 4, 0.178);
+  margin-top: 30px;
+  padding: 0 20px;
+  border-style: solid;
+  border-width: 2px 0;
+  border-color: rgba(255, 235, 205, 0.288);
+  background-color: rgba(15, 1, 4, 0.178);
 }
 
-h3 , h4 {
-    color:rgb(240, 159, 94)
-  }
+h3,
+h4 {
+  color: rgb(240, 159, 94);
+}
 
 table {
   width: 100%;
-}
-
-td {
-  min-width: 33%;
 }
 
 .header-row {
@@ -78,5 +108,9 @@ td {
   border-style: solid;
   border-width: 0 0 1px;
 }
-    
+
+
+.fat {
+  font-weight: bolder;
+}
 </style>
