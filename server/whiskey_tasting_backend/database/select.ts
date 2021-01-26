23f852ee14fade782ff6e23b253137bc8a7cf422
Kevin Pandura <pandu.kev.kp@gmail.com>
2021-01-26 20:02:47 +0100

    refactor: :art: added types to variables, parameters and returns

--- server/whiskey_tasting_backend/database/select.ts
+++ server/whiskey_tasting_backend/database/select.ts
@@ -2,44 +2,46 @@ var db = require('./connection.ts');
 
 //WHISKEY SELECTS
 
-exports.allWhiskeys = function (callback) {
-    let query = 'SELECT * FROM whiskeys';
-    let result = db.query(query, callback);
+exports.allWhiskeys = function (callback: Function) : Array<Object> {
+    let query: string = 'SELECT * FROM whiskeys ORDER BY name';
+    let result: Array<Object> = db.query(query, callback);
     return result;
 }
 
-exports.whiskeyById = function (id, callback) {
-    let query = 'SELECT * FROM whiskeys WHERE id=' + id;
-    let result = db.query(query, callback);
+exports.whiskeyById = function (id: number, callback: Function) : Array<Object> {
+    let query: string = 'SELECT * FROM whiskeys WHERE id=' + id;
+    let result: Array<Object> = db.query(query, callback);
     return result;
 }
 
-exports.whiskeySearchByName = function (searchPhrase, callback) {
-    let query = 'SELECT * FROM whiskeys WHERE name LIKE "%' + searchPhrase + '%"';
+exports.whiskeySearchByName = function (searchPhrase: string, callback: Function) : Array<Object> {
+    let query: string = 'SELECT * FROM whiskeys WHERE name LIKE "%' + searchPhrase + '%"';
     console.log('searching for : ' + searchPhrase);
-    let result = db.query(query, callback);
+    let result: Array<Object> = db.query(query, callback);
     return result;
 }
 
-exports.whiskeyByTastingId = function (tastingId, callback) {
-    let query = 'SELECT * FROM whiskeys WHERE TASTING_ID = ' + tastingId + ' ORDER BY total_rating';
+exports.whiskeyByTastingId = function (tastingId: number, callback: Function) : Array<Object> {
+    let query: string = 'SELECT * FROM whiskeys WHERE TASTING_ID = ' + tastingId + ' ORDER BY total_rating desc';
     console.log('requesting whiskeys for tasting with id: ' + tastingId);
-    let result = db.query(query, callback);
+    let result: Array<Object> = db.query(query, callback);
     return result;
 }
 
 
 //TASTING SELECTS 
 
-exports.allTastings = function (callback) {
-    let query = 'SELECT id, DATE_FORMAT(date, "%Y %m %d") AS date FROM tasting ORDER BY date desc';
-    let results = db.query(query, callback);
+exports.allTastings = function (callback: Function ) : Array<Object> {
+    let query: string = 'SELECT id, DATE_FORMAT(date, "%Y %m %d") AS date FROM tasting ORDER BY date desc';
+    console.log('requesting all tastings');
+    let results: Array<Object> = db.query(query, callback);
     return results;
 }
 
-exports.tastingByDate = function (date, callback) {
-    let query = 'SELECT id, DATE_FORMAT(date, "%a %d.%m.%Y") AS date FROM tasting ORDER BY ABS( DATEDIFF( date , "' + date + '" ) ) LIMIT 1';
-    let result = db.query(query, callback);
+exports.tastingByDate = function (date: string, callback: Function) : Object {
+    let query: string = 'SELECT id, DATE_FORMAT(date, "%a %d.%m.%Y") AS date FROM tasting ORDER BY ABS( DATEDIFF( date , "' + date + '" ) ) LIMIT 1';
+    console.log('requesting a tasting tat is neares to the date: ' + date);
+    let result: Array<Object> = db.query(query, callback);
     if (result.length > 0) {
         return result[0];
     }
