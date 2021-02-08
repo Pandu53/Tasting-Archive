6e9b3d032f2e3e4ee2531ab02cb4ce623ad834a4
Kevin Pandura <pandu.kev.kp@gmail.com>
2021-02-08 23:17:56 +0100

    feat: :sparkles: added cors and vuex to the project

--- client/whiskey-tasting-frontend/src/main.ts
+++ client/whiskey-tasting-frontend/src/main.ts
@@ -2,11 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import store from "./store"
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = 'http://localhost:3000';
+
+
 
 createApp(App)
   .use(router)
+  .use(store)
   .mount("#app");
---
+++ client/whiskey-tasting-frontend/src/store.ts
@@ -0,0 +1,10 @@
+import { createStore } from "vuex" 
+
+const store = createStore({
+   state:{
+      whiskeyList: [],
+      tatsingList: [],
+   }
+})
+
+export default store
